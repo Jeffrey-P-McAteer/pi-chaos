560a3f8b111d81a9d188494145ab1bced9bda344
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-05-19 14:11:50 -0400

    ok, got a slow but not-terribly-stupid answer. Good news, the complex stuff zeroes itself!

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ except:
   ])
   import numba
 
-@numba.jit(nopython=True)
+@numba.jit(nopython=True, fastmath=True, cache=True)
 def leibniz_seq(k):
   return (( (-1) ** k ) * 4.0 ) / ((2*k) + 1)
 
@@ -29,8 +29,38 @@ def leibniz_seq(k):
 def leibniz_sum_to(term_num):
   return sum([leibniz_seq(k) for k in range(0, term_num)])
 
+
+#@numba.jit(nopython=True, fastmath=True, cache=True)
+@numba.jit(nopython=False, fastmath=False, cache=False)
+def leibniz_squared_seq(k):
+  value = ( (( (-1) ** k ) * 4.0 ) / ((2*k) + 1) ) ** ( (( (-1) ** k ) * 4.0 ) / ((2*k) + 1) )
+  if isinstance(value, complex):
+    return value
+  if not math.isnan(value):
+    return complex(value, 0.0)
+  return complex(0.0, 0.0)
+
+@numba.jit(nopython=False, fastmath=False, cache=False)
+def leibniz_squared_sum_to(term_num):
+  complex_values = [leibniz_squared_seq(k) for k in range(0, term_num)]
+  complex_sum = sum([v.real for v in complex_values])
+  imaginary_sum = sum([v.imag for v in complex_values])
+  return (complex_sum, imaginary_sum)
+
+
 def main(args=sys.argv):
-  print(f'leibniz_sum_to(5000000) = {leibniz_sum_to(5000000)}')
+  leibniz_num_terms = 5000000
+  leibniz_pi_approx = leibniz_sum_to(leibniz_num_terms)
+  print(f'leibniz_sum_to({leibniz_num_terms}) = {leibniz_pi_approx}')
+
+  pi_squared_approx = leibniz_pi_approx ** leibniz_pi_approx
+  print(f'pi_squared_approx = {pi_squared_approx}')
+
+  leibniz_squared_pi_approx = leibniz_squared_sum_to(leibniz_num_terms)
+  print(f'leibniz_squared_pi_approx = {leibniz_squared_pi_approx}')
+
+
+
 
 
 
